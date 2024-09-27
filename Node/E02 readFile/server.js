85d4852d74cbb292519f3c14dc6b9710f8ec5a49
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-09-27 11:40:29 +0200

    Node - E02 readFile

--- Node/E02 readFile/server.js
+++ Node/E02 readFile/server.js
@@ -2,7 +2,7 @@ import http from 'http';
 import url from 'url';
 import fs from 'fs';
 import mime from 'mime';
-import headers from './headers.json';
+import headers from './headers.json' with {type: 'json'};
 
 const PORT = 1337;
 
@@ -10,6 +10,7 @@ const server = http.createServer(function (req, res) {
   let { method } = req;
   let fullPath = url.parse(req.url, true);
   let resource = fullPath.pathname;
+  // fullPath.query contiene solo i parametri GET
   let params = fullPath.query;
 
   console.log(`Richiesta ${method}: ${resource}`);
@@ -18,7 +19,7 @@ const server = http.createServer(function (req, res) {
     resource = '/index.html';
   }
 
-  if (!resource.startsWith('/api/')) {
+  if (!resource.startsWith('/api/')) { // Risorsa statica
     resource = `./static${resource}`;
     fs.readFile(resource, function (err, data) {
       if (!err) {
@@ -27,11 +28,34 @@ const server = http.createServer(function (req, res) {
         res.write(data);
         res.end();
       } else {
-        res.writeHead(404, headers.text);
-        res.write('Risorsa non trovata!');
-        res.end();
+        fs.readFile('./static/error.html', function(err, data) {
+          if(err) {
+            res.writeHead(404, headers.text);
+            res.write('Pagina non trovata!');
+          }
+          else {
+            res.writeHead(404, headers.html);
+            res.write(data);
+          }
+          res.end();
+        })
       }
     });
+  }
+  else if(resource == '/api/servizio1') {
+    res.writeHead(200, headers.json);
+    res.write(JSON.stringify({message: `Benvenuto: ${params.nome}`}));
+    res.end();
+  }
+  else if(resource == '/api/servizio2') {
+    res.writeHead(200, headers.json);
+    res.write(JSON.stringify({message: `Benvenuto: ${params.nome}`}));
+    res.end();
+  }
+  else {
+    res.writeHead(404, headers.text);
+    res.write('API non disponibile!');
+    res.end();
   }
 });
 
--- Node/E02 readFile/static/index.js
+++ Node/E02 readFile/static/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         let data = await inviaRichiesta("get", "/api/servizio1", {"nome":"pippo"})
 		console.log(data)
 		if (data) {
-			alert ("ok")
+			alert (data.message)
 		}
     });
 
@@ -12,7 +12,7 @@ $(document).ready(function() {
         let data = await inviaRichiesta("post", "/api/servizio2", {"nome":"pluto"})
 		console.log(data)
 		if (data) {
-			alert ("ok")
+			alert (data.message)
 		}
     });
 	
