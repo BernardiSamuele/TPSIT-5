30e3136ad4b14039e61ff57b61a6a1589263b05e
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-11-12 16:12:36 +0100

    Angular - E01

---
+++ Angular/ES_01/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Es_Bernardi';
+  isLoggedIn = true;
+  operatingSystems = [
+    { id: 'win', name: 'Windows' },
+    { id: 'osx', name: 'MacOS' },
+    { id: 'linux', name: 'Linux' },
+  ];
+  isEditable = true;
+}
