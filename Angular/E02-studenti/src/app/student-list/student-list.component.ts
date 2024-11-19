7c352cc21e19edc513d518bbb1ab58b94fd6c4b6
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-11-19 13:05:09 +0100

    Angular - E02 students

--- Angular/E02-studenti/src/app/student-list/student-list.component.ts
+++ Angular/E02-studenti/src/app/student-list/student-list.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { StudentComponent } from '../student/student.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [StudentComponent],
+  imports: [StudentComponent, FormsModule],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css',
 })
 export class StudentListComponent {
+  @ViewChild('txtName') _txtName!: ElementRef;
   studentList: any[] = [
     { name: 'pippo', city: 'Fossano', gender: 'M', present: true },
     { name: 'pluto', city: 'Cuneo', gender: 'M', present: true },
@@ -23,5 +25,37 @@ export class StudentListComponent {
     { name: 'jack', city: 'Fossano', gender: 'M', present: true },
   ];
 
+  ngAfterViewInit() {
+    this._txtName.nativeElement.focus();
+  }
+
+  onCreateStudent() {
+    console.log(this.studentName, this.studentGender, this.studentCity);
+    let newStudent = {
+      name: this.studentName,
+      gender: this.studentGender,
+      city: this.studentCity,
+      present: false,
+    };
+
+    this.studentList.push(newStudent);
+    this.studentName = '';
+    this.studentGender = 'M';
+    this.studentCity = '';
+
+    this._txtName.nativeElement.focus();
+  }
+
+  // onDeleteStudent(pos: number) {
+  //   this.studentList.splice(pos, 1);
+  // }
+  onDeleteStudent(student: any) {
+    let pos = this.studentList.indexOf(student);
+    this.studentList.splice(pos, 1);
+  }
+
   cities: string[] = ['Fossano', 'Torino', 'Cuneo', 'Alba', 'Canale'];
+  studentName: any;
+  studentGender: any = 'M';
+  studentCity: any;
 }
